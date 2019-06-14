c7b0eff37f0540c43fe21acdf2ca66e08bcf775f
Morgane DUBUS <mdubus@student.42.fr>
2019-06-14 14:50:19 +0200

    base project

---
+++ Swifty Protein/Swifty Protein/GameViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GameViewController.swift
+//  Swifty Protein
+//
+//  Created by Morgane DUBUS on 6/14/19.
+//  Copyright © 2019 Morgane DUBUS. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+}
---
+++ Swifty Protein/Swifty Protein/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  Swifty Protein
+//
+//  Created by Morgane DUBUS on 6/14/19.
+//  Copyright © 2019 Morgane DUBUS. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+}
+
