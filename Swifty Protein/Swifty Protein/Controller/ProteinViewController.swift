a545dd214f86b16d1685f2ed46c805631ae0e6c5
Adrien FOURCADE <afourcad@e3r13p14.42.fr>
2019-06-20 17:42:48 +0200

    add atoms to proteins

--- Swifty Protein/Swifty Protein/Controller/ProteinViewController.swift
+++ Swifty Protein/Swifty Protein/Controller/ProteinViewController.swift
@@ -8,20 +8,24 @@ class ProteinViewController: UIViewController {
     var scnScene: SCNScene!
     var cameraNode: SCNNode!
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+//    var molecule: Molecules!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         setupView()
         setupScene()
         setupCamera()
         spawnAtom()
-//        CreateMolecule(moleculePdb: "ATOM      1  CHA HEM A   1       2.748 -19.531  39.896  1.00 10.00           C")
-        createAtom(atomPdb: "ATOM      1  CHA HEM A   1       2.748 -19.531  39.896  1.00 10.00           C")
-        createAtom(atomPdb: "ATOM      2  CHB HEM A   1       3.258 -17.744  35.477  1.00 10.00           C")
-        fetchAll()
+        
+        
+        let pdbFile = "ATOM      1  CHA HEM A   1       2.748 -19.531  39.896  1.00 10.00           C\nATOM      2  CHB HEM A   1       3.258 -17.744  35.477  1.00 10.00           C"
+        
         deleteAllData("Atoms")
         deleteAllData("Molecules")
+        createMolecule(moleculePdb: pdbFile)
+        fetchAll()
     }
     
     override var shouldAutorotate: Bool {
@@ -39,27 +43,19 @@ class ProteinViewController: UIViewController {
     func setupScene() {
         scnScene = SCNScene()
         scnView.scene = scnScene
-        // 1
         scnView.showsStatistics = true
-        // 2
         scnView.allowsCameraControl = true
-        // 3
         scnView.autoenablesDefaultLighting = true
     }
     
     func setupCamera() {
-        // 1
         cameraNode = SCNNode()
-        // 2
         cameraNode.camera = SCNCamera()
-        // 3
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 60)
-        // 4
         scnScene.rootNode.addChildNode(cameraNode)
     }
     
     func spawnAtom() {
-        // 1
         var geometry: SCNGeometry
         var geometry2: SCNGeometry
         let pos1 = simd_float3(x:2, y:5, z:6)
@@ -69,12 +65,9 @@ class ProteinViewController: UIViewController {
         let norm = simd_normalize(diff)
         let dot = simd_dot(yAxis, norm)
         
-        
-        
         geometry = SCNSphere(radius: 0.6)
         geometry2 = SCNCylinder(radius: 0.3, height: 1)
         
-        // 4
         let geometryNode1 = SCNNode(geometry: geometry)
         let geometryNode2 = SCNNode(geometry: geometry)
         let geometryNode3 = SCNNode(geometry: geometry2)
@@ -90,89 +83,50 @@ class ProteinViewController: UIViewController {
         geometryNode2.simdPosition = pos2
         geometryNode3.simdPosition = diff / 2 + pos1
         geometryNode3.simdScale = simd_float3(x: 1, y: simd_length(diff), z: 1)
-        // 5
         
         scnScene.rootNode.addChildNode(geometryNode1)
         scnScene.rootNode.addChildNode(geometryNode2)
         scnScene.rootNode.addChildNode(geometryNode3)
-        
-        
     }
     
-    func createAtom(atomPdb: String) -> Atoms{
+    func createAtom(atomPdb: String, molecule : Molecules){
         let atom = Atoms(context: context)
-        let splitAtomLine = atomPdb.split(separator: " ")
-        
+        let splitAtomLine = atomPdb.split(separator: " ", omittingEmptySubsequences: true)
+
         atom.name = String(splitAtomLine[11])
         atom.atom_Id = String(splitAtomLine[2])
-        atom.coor_X = Float(splitAtomLine[6])!
-        atom.coor_Y = Float(splitAtomLine[7])!
-        atom.coor_Z = Float(splitAtomLine[8])!
+        atom.coor_X = Float(String(splitAtomLine[6]))!
+        atom.coor_Y = Float(String(splitAtomLine[7]))!
+        atom.coor_Z = Float(String(splitAtomLine[8]))!
         
-        do{
-            try context.save()
-            print("Atoms bien sauvegardé")
-        }catch let error{
-            print(error)
-        }
-        
-        return atom
+        molecule.addToAtom(atom)
     }
     
-//    func createLink(newLink: Array<Substring>){
-//        /*
-//            newLink[1] est l atom de ref, les suivant sont ses connections.
-//            si l'id des suivant est superieur a celui de ref alors ont inscrit une nouvelle connection.
-//            sinon elle a logiquement deja été inscrite
-//        */
-//
-//        let firstId = Int(newLink[1])!
-//        for secondId in 2..<newLink.count{
-//            if (firstId < Int(secondId)){
-//                molecule.links.append((firstId, Int(secondId)))
-//            }
-//        }
-//    }
-//
-//    func CreateMolecule(moleculePdb: String){
-//
-//        //separation du fichier pdb par ligne
-//        let moleculePdbLines = moleculePdb.split(separator: "\n")
-//        var lineTmp: Array<Substring>!
-//
-//        for line in moleculePdbLines{
-//            lineTmp = line.split(separator: " ")
-//
-//            if (lineTmp[0] == "ATOM"){
-//                molecule.structure.append(createAtom(atomPdb: lineTmp))
-//            }
-//            else if (lineTmp[0] == "CONECT"){
-//                createLink(newLink: lineTmp)
-//            }
-//            else{
-//                print("End of file\n")
-//            }
-//        }
-////        var geometry: SCNGeometry
-////
-////        geometry = SCNSphere(radius: 0.6)
-////        let geometryNode1 = SCNNode(geometry: geometry)
-////        geometryNode1.position = SCNVector3(x:2, y:5, z:6)
-////        scnScene.rootNode.addChildNode(geometryNode1)
-//
-//    }
-  
-    
-    func createMolecule(){
+    func createMolecule(moleculePdb: String){
         let molecule = Molecules(context: context)
-        
         molecule.name = "proteineDeOuf"
         molecule.ligand_Id = "jlkjlkj"
         
-        do{
+        let pdbLines = moleculePdb.split(separator: "\n", omittingEmptySubsequences: true)
+        var lineTmp: Array<Substring>!
+        
+        for line in pdbLines{
+            lineTmp = line.split(separator: " ",omittingEmptySubsequences: true)
+
+            if (lineTmp[0] == "ATOM"){
+                createAtom(atomPdb: String(describing: line), molecule: molecule)
+            }
+//            else if (lineTmp[0] == "CONECT"){
+//            }
+            else{
+                print("End of file\n")
+            }
+        }
+        
+        do {
             try context.save()
             print("bien sauvegardé")
-        }catch let error{
+        } catch let error{
             print(error)
         }
     }
@@ -180,13 +134,18 @@ class ProteinViewController: UIViewController {
     func fetchAll(){
         let request: NSFetchRequest<Molecules> = Molecules.fetchRequest()
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        
+
         guard let molecules = try? context.fetch(request) else {
+            print("Error fetching molecules")
             return
         }
         
-        for molecule in molecules{
+        for molecule in molecules {
             print(molecule.name)
+        
+            for atom in molecule.atom as! Set<Atoms> {
+                print(atom.atom_Id)
+            }
         }
     }
     
@@ -207,5 +166,31 @@ class ProteinViewController: UIViewController {
     }
 }
 
+
+//    func createLink(newLink: Array<Substring>){
+//        /*
+//            newLink[1] est l atom de ref, les suivant sont ses connections.
+//            si l'id des suivant est superieur a celui de ref alors ont inscrit une nouvelle connection.
+//            sinon elle a logiquement deja été inscrite
+//        */
+//
+//        let firstId = Int(newLink[1])!
+//        for secondId in 2..<newLink.count{
+//            if (firstId < Int(secondId)){
+//                molecule.links.append((firstId, Int(secondId)))
+//            }
+//        }
+//    }
+//
+//    func AddOneSphere(moleculePdb: String){
+//
+//        var geometry: SCNGeometry
+//
+//        geometry = SCNSphere(radius: 0.6)
+//        let geometryNode1 = SCNNode(geometry: geometry)
+//        geometryNode1.position = SCNVector3(x:2, y:5, z:6)
+//        scnScene.rootNode.addChildNode(geometryNode1)
+//
+//    }
 
 
